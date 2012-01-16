57c5644b2764a13d59cb01a8fc56b1387f1ac811
Jason Bucki <jasonbucki@gmail.com>
2012-01-15 23:09:26 -0600

    finishing up sequence comprehensions

--- src/koans/runtime_polymorphism.clj
+++ src/koans/runtime_polymorphism.clj
@@ -7,28 +7,34 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
+(defmethod diet :default [a] "I don't know what Rich Hickey eats.")
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
   (= "Bambi eats veggies."
-     (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
+     (diet {:species "deer"
+            :name "Bambi"
+            :age 1
+            :eater :herbivore}))
 
   "Different methods are used depending on the dispatch function result"
   (= "Simba eats animals."
-     (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
+     (diet {:species "lion"
+            :name "Simba"
+            :age 1
+            :eater :carnivore}))
 
   "You may use a default method when no others match"
   (= "I don't know what Rich Hickey eats."
